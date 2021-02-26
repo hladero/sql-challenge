dc179b5d94d8038e38522774accdf08c0c38b312
hector ladero <hectorladero@gmail.com>
2021-02-25 20:41:11 -0500

    Queries for databases

--- Employeesql/Database.sql
+++ Employeesql/Database.sql
@@ -28,7 +28,7 @@ Create table  employees (
 	first_name VARCHAR,
 	last_name VARCHAR,
 	sex	VARCHAR,
-	hire_date VARCHAR);
+	hire_date DATE);
 DROP TABLE employees;
 Select * from employees;
 --insert primary key
@@ -47,6 +47,26 @@ Create table  titles (
 	title_id VARCHAR,
 	title VARCHAR
 	);
-Select * from titles;	
-	
+Select * from titles;
+
+--1. List the following details of each employee: employee number, last name, first name, sex, and salary.
+select employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
+from employees
+join salaries
+on employees.emp_no = salaries.emp_no;
+
+--2. List first name, last name, and hire date for employees who were hired in 1986.
+select first_name, last_name, hire_date
+from employees 
+where hire_date between '1986-01-01' and '1986-12-31' 
+ORDER BY hire_date ASC
+
+--3. List the manager of each department with the following information: 
+--department number, department name, the manager's employee number, last name, first name.
+select departments.dept_no,departments.dept_name, dept_manager.emp_no, employees.first_name, employees.last_name
+from departments
+join dept_manager
+on departments.dept_no = dept_manager.dept_no
+join employees
+on dept_manager.emp_no = employees.emp_no;
 
